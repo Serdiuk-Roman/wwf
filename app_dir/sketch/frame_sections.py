9169608c81e3d72d03ddfe830c6518cfe23c8c12
Serdiuk-Roman <serdiuk.r@gmail.com>
2020-09-06 16:15:02 +0300

    update frame sketch

--- app_dir/sketch/frame_sections.py
+++ app_dir/sketch/frame_sections.py
@@ -1,5 +1,5 @@
 from app_dir.sketch.sketch_config import CM, black, g8, Small_line_width,\
-    centered_text, centered_vertical_text
+    centered_text, centered_vertical_text, white
 
 class FramePainter:
 
@@ -20,54 +20,83 @@ def get_painter(frame_type):
 def _draw_ww_external(ctx, sx, sy):
 
     ww_external_points = (
-        (2, 0),
-        (-1, -1),
-        (0, -4),
-        (10, -10),
-        (0, -4),
-        (4, 0),
-        (0, 4),
         (5, 0),
-        (0, -1),
-        (-4, 0),
-        (0, -5),
-        (15, 0),
-        (0, 5),
-        (-4, 0),
         (0, 1),
-        (5, 0),
-        (0, -10),
-        (-5, 0),
-        (0, -5),
-        (5, 0),
-        (0, -25),
-        (-5, 0),
-        (0, -5),
-        (5, 0),
-        (0, -10),
-        (-15, 0),
-        (0, 1),
-        (4, 0),
+        (-4, 0),
         (0, 5),
-        (-15, 0),
+        (16, 0),
         (0, -5),
-        (4, 0),
+        (-4, 0),
         (0, -1),
+        (17, 0),
+        (0, 8),
         (-5, 0),
-        (0, 10),
-        (-14, 0),
+        (0, 3),
+        (5, 0),
+        (0, 27),
+        (-5, 0),
+        (0, 3),
+        (5, 0),
+        (0, 9),
+        (-5, 0),
+        (0, -1),
+        (4, 0),
+        (0, -5),
+        (-16, 0),
+        (0, 5),
+        (4, 0),
+        (0, 1),
+        (-5, 0),
         (0, -3),
-        (1, -1),
+        (-4, 0),
+        (0, 3),
+        (-9, 9),
+        (0, 3),
+        (1, 1),
         (-2, 0),
+        (0, -42),
+        (5, 0),
+        (2, -1),
+        (1, -1.5),
+        (-1, -1.5),
+        (-2, -1),
+        (-17, 0),
+        (0, -3),
+        (2, 0),
+        (-1, 1),
+        (0, 1),
+        (13, 0)
+    )
+
+    ww_internal_points = (
+        (17, 0),
+        (0, -6),
+        (11, 0),
+        (0, 6),
+        (-5, 0),
         (0, 5),
-        (13, 0),
-        (2, 2),
-        (0, 2),
-        (-2, 2),
+        (5, 0),
+        (0, 25),
+        (-5, 0),
+        (0, 5),
+        (-13, 0),
+        (0, 3),
+        (-3, 0),
+        (0, -12),
+        (-3, 0),
+        (-2, 6),
+        (0, 13),
+        (-3, 3),
+        (0, -33),
+        (4, 0),
+        (2, -1),
+        (1, -2.5),
+        (-1, -2.5),
+        (-2, -1),
         (-3, 0)
     )
 
-    ctx.move_to(sx + CM, sy + 3 * CM)
+    ctx.move_to(sx + 0.5 * CM + 14, sy + 0.5 * CM)
 
     for point in ww_external_points:
         ctx.rel_line_to(*point)
@@ -78,14 +107,27 @@ def _draw_ww_external(ctx, sx, sy):
     ctx.set_line_width(Small_line_width)
     ctx.set_source_rgb(*black)
     ctx.stroke()
+
+    ctx.move_to(sx + 0.5 * CM + 15, sy + 0.5 * CM + 7)
+
+    for point in ww_internal_points:
+        ctx.rel_line_to(*point)
+
+    ctx.close_path()
+    ctx.set_source_rgb(*white)
+    ctx.fill_preserve()
+    ctx.set_line_width(Small_line_width)
+    ctx.set_source_rgb(*black)
+    ctx.stroke()
+
     ctx.set_font_size(8)
     centered_text(
         ctx, sx / CM, sy / CM, 2.5, 0.5,
         text="Сечение лутки"
     )
-    ctx.set_font_size(14)
+    ctx.set_font_size(10)
     centered_vertical_text(
-        ctx, sx / CM, sy / CM + 1, 1, 2,
+        ctx, sx / CM, sy / CM + 1.5, 1, 1,
         text="Наружная"
     )
 
